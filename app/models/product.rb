0b7dbf6a3c2492de5e781de09671aaf2ed82fe9d
Salim Mwatsefu Mwarika <sjmwatsefu@gmail.com>
2022-11-06 23:41:19 +0300

    done

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,18 @@
 class Product < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
+
+    def leave_review(user, star_rating, comment)
+        self.reviews.create(user: user, star_rating: star_rating, comment: comment)
+    end
     
+    def print_all_reviews 
+        self.reviews.each do |x|
+            puts "Review for #{x.product.name} by #{x.user.name}: #{x.star_rating}. #{x.comment}"
+        end
+    end
+
+    def average_rating
+        self.reviews.average(:star_rating)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,13 @@ class User < ActiveRecord::Base
     has_many :reviews
     has_many :products, through: :reviews
 
+    def favorite_product
+        self.reviews.all.sort{ |x,y| x.star_rating <=> y.star_rating }.last
+    end
+
+    def remove_reviews product
+        self.products.find_by(id: product.id).reviews.destroy_all
+    end
+
     
 end
